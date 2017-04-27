1e1bf2af61cdff69926a68c7314abe03332d95d6
naidu540 <kumilinaidu@gmail.com>
2017-04-27 07:26:45 -0600

    Added new variables

--- Programs/ArthiOperations.java
+++ Programs/ArthiOperations.java
@@ -5,6 +5,7 @@ public class ArthiOperations
 	public int salary = 10000;
     String name = "Naidu";
     String kid = "Aarohi";
+    String wifey = "shravs";
    public static void main(String[] args) 
    {
 	int sub,add,mul,div,i,j;
